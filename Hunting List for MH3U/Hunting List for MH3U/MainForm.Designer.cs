c86964b8b670353f6054704fb33ecaac51de05c7
ampedPF <ampedpf@gmail.com>
2014-07-07 02:14:38 +0200

    Main interface set up

--- Hunting List for MH3U/Hunting List for MH3U/MainForm.Designer.cs
+++ Hunting List for MH3U/Hunting List for MH3U/MainForm.Designer.cs
@@ -28,20 +28,286 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.gbFilters = new System.Windows.Forms.GroupBox();
+            this.gbAmrFilters = new System.Windows.Forms.GroupBox();
+            this.gbWpnFilters = new System.Windows.Forms.GroupBox();
+            this.chkAmrHead = new System.Windows.Forms.CheckBox();
+            this.chkAmrBody = new System.Windows.Forms.CheckBox();
+            this.chkAmrArms = new System.Windows.Forms.CheckBox();
+            this.chkAmrWaist = new System.Windows.Forms.CheckBox();
+            this.chkAmrLegs = new System.Windows.Forms.CheckBox();
+            this.chkWpnGS = new System.Windows.Forms.CheckBox();
+            this.chkWpnLS = new System.Windows.Forms.CheckBox();
+            this.chkWpnSnS = new System.Windows.Forms.CheckBox();
+            this.chkWpnDS = new System.Windows.Forms.CheckBox();
+            this.chkWpnHammer = new System.Windows.Forms.CheckBox();
+            this.chkWpnHH = new System.Windows.Forms.CheckBox();
+            this.chkWpnLance = new System.Windows.Forms.CheckBox();
+            this.chkWpnGL = new System.Windows.Forms.CheckBox();
+            this.chkWpnSA = new System.Windows.Forms.CheckBox();
+            this.chkWpnLBG = new System.Windows.Forms.CheckBox();
+            this.chkWpnHBG = new System.Windows.Forms.CheckBox();
+            this.chkWpnBow = new System.Windows.Forms.CheckBox();
+            this.gbFilters.SuspendLayout();
+            this.gbAmrFilters.SuspendLayout();
+            this.gbWpnFilters.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // gbFilters
+            // 
+            this.gbFilters.Controls.Add(this.gbWpnFilters);
+            this.gbFilters.Controls.Add(this.gbAmrFilters);
+            this.gbFilters.Location = new System.Drawing.Point(12, 12);
+            this.gbFilters.Name = "gbFilters";
+            this.gbFilters.Size = new System.Drawing.Size(915, 341);
+            this.gbFilters.TabIndex = 0;
+            this.gbFilters.TabStop = false;
+            this.gbFilters.Text = "Filters";
+            // 
+            // gbAmrFilters
+            // 
+            this.gbAmrFilters.Controls.Add(this.chkAmrLegs);
+            this.gbAmrFilters.Controls.Add(this.chkAmrWaist);
+            this.gbAmrFilters.Controls.Add(this.chkAmrArms);
+            this.gbAmrFilters.Controls.Add(this.chkAmrBody);
+            this.gbAmrFilters.Controls.Add(this.chkAmrHead);
+            this.gbAmrFilters.Location = new System.Drawing.Point(6, 19);
+            this.gbAmrFilters.Name = "gbAmrFilters";
+            this.gbAmrFilters.Size = new System.Drawing.Size(291, 50);
+            this.gbAmrFilters.TabIndex = 0;
+            this.gbAmrFilters.TabStop = false;
+            this.gbAmrFilters.Text = "Armors";
+            // 
+            // gbWpnFilters
+            // 
+            this.gbWpnFilters.Controls.Add(this.chkWpnBow);
+            this.gbWpnFilters.Controls.Add(this.chkWpnHBG);
+            this.gbWpnFilters.Controls.Add(this.chkWpnLBG);
+            this.gbWpnFilters.Controls.Add(this.chkWpnSA);
+            this.gbWpnFilters.Controls.Add(this.chkWpnGL);
+            this.gbWpnFilters.Controls.Add(this.chkWpnLance);
+            this.gbWpnFilters.Controls.Add(this.chkWpnHH);
+            this.gbWpnFilters.Controls.Add(this.chkWpnHammer);
+            this.gbWpnFilters.Controls.Add(this.chkWpnDS);
+            this.gbWpnFilters.Controls.Add(this.chkWpnSnS);
+            this.gbWpnFilters.Controls.Add(this.chkWpnLS);
+            this.gbWpnFilters.Controls.Add(this.chkWpnGS);
+            this.gbWpnFilters.Location = new System.Drawing.Point(303, 19);
+            this.gbWpnFilters.Name = "gbWpnFilters";
+            this.gbWpnFilters.Size = new System.Drawing.Size(606, 207);
+            this.gbWpnFilters.TabIndex = 1;
+            this.gbWpnFilters.TabStop = false;
+            this.gbWpnFilters.Text = "Weapons";
+            // 
+            // chkAmrHead
+            // 
+            this.chkAmrHead.AutoSize = true;
+            this.chkAmrHead.Location = new System.Drawing.Point(7, 20);
+            this.chkAmrHead.Name = "chkAmrHead";
+            this.chkAmrHead.Size = new System.Drawing.Size(52, 17);
+            this.chkAmrHead.TabIndex = 0;
+            this.chkAmrHead.Text = "Head";
+            this.chkAmrHead.UseVisualStyleBackColor = true;
+            // 
+            // chkAmrBody
+            // 
+            this.chkAmrBody.AutoSize = true;
+            this.chkAmrBody.Location = new System.Drawing.Point(65, 20);
+            this.chkAmrBody.Name = "chkAmrBody";
+            this.chkAmrBody.Size = new System.Drawing.Size(50, 17);
+            this.chkAmrBody.TabIndex = 1;
+            this.chkAmrBody.Text = "Body";
+            this.chkAmrBody.UseVisualStyleBackColor = true;
+            // 
+            // chkAmrArms
+            // 
+            this.chkAmrArms.AutoSize = true;
+            this.chkAmrArms.Location = new System.Drawing.Point(122, 20);
+            this.chkAmrArms.Name = "chkAmrArms";
+            this.chkAmrArms.Size = new System.Drawing.Size(49, 17);
+            this.chkAmrArms.TabIndex = 2;
+            this.chkAmrArms.Text = "Arms";
+            this.chkAmrArms.UseVisualStyleBackColor = true;
+            // 
+            // chkAmrWaist
+            // 
+            this.chkAmrWaist.AutoSize = true;
+            this.chkAmrWaist.Location = new System.Drawing.Point(178, 20);
+            this.chkAmrWaist.Name = "chkAmrWaist";
+            this.chkAmrWaist.Size = new System.Drawing.Size(53, 17);
+            this.chkAmrWaist.TabIndex = 3;
+            this.chkAmrWaist.Text = "Waist";
+            this.chkAmrWaist.UseVisualStyleBackColor = true;
+            // 
+            // chkAmrLegs
+            // 
+            this.chkAmrLegs.AutoSize = true;
+            this.chkAmrLegs.Location = new System.Drawing.Point(238, 20);
+            this.chkAmrLegs.Name = "chkAmrLegs";
+            this.chkAmrLegs.Size = new System.Drawing.Size(49, 17);
+            this.chkAmrLegs.TabIndex = 4;
+            this.chkAmrLegs.Text = "Legs";
+            this.chkAmrLegs.UseVisualStyleBackColor = true;
+            // 
+            // chkWpnGS
+            // 
+            this.chkWpnGS.AutoSize = true;
+            this.chkWpnGS.Location = new System.Drawing.Point(7, 20);
+            this.chkWpnGS.Name = "chkWpnGS";
+            this.chkWpnGS.Size = new System.Drawing.Size(85, 17);
+            this.chkWpnGS.TabIndex = 0;
+            this.chkWpnGS.Text = "Great Sword";
+            this.chkWpnGS.UseVisualStyleBackColor = true;
+            // 
+            // chkWpnLS
+            // 
+            this.chkWpnLS.AutoSize = true;
+            this.chkWpnLS.Location = new System.Drawing.Point(99, 20);
+            this.chkWpnLS.Name = "chkWpnLS";
+            this.chkWpnLS.Size = new System.Drawing.Size(83, 17);
+            this.chkWpnLS.TabIndex = 1;
+            this.chkWpnLS.Text = "Long Sword";
+            this.chkWpnLS.UseVisualStyleBackColor = true;
+            // 
+            // chkWpnSnS
+            // 
+            this.chkWpnSnS.AutoSize = true;
+            this.chkWpnSnS.Location = new System.Drawing.Point(189, 20);
+            this.chkWpnSnS.Name = "chkWpnSnS";
+            this.chkWpnSnS.Size = new System.Drawing.Size(101, 17);
+            this.chkWpnSnS.TabIndex = 2;
+            this.chkWpnSnS.Text = "Sword \'n\' Shield";
+            this.chkWpnSnS.UseVisualStyleBackColor = true;
+            // 
+            // chkWpnDS
+            // 
+            this.chkWpnDS.AutoSize = true;
+            this.chkWpnDS.Location = new System.Drawing.Point(297, 20);
+            this.chkWpnDS.Name = "chkWpnDS";
+            this.chkWpnDS.Size = new System.Drawing.Size(86, 17);
+            this.chkWpnDS.TabIndex = 3;
+            this.chkWpnDS.Text = "Dual Swords";
+            this.chkWpnDS.UseVisualStyleBackColor = true;
+            // 
+            // chkWpnHammer
+            // 
+            this.chkWpnHammer.AutoSize = true;
+            this.chkWpnHammer.Location = new System.Drawing.Point(395, 19);
+            this.chkWpnHammer.Name = "chkWpnHammer";
+            this.chkWpnHammer.Size = new System.Drawing.Size(65, 17);
+            this.chkWpnHammer.TabIndex = 4;
+            this.chkWpnHammer.Text = "Hammer";
+            this.chkWpnHammer.UseVisualStyleBackColor = true;
+            // 
+            // chkWpnHH
+            // 
+            this.chkWpnHH.AutoSize = true;
+            this.chkWpnHH.Location = new System.Drawing.Point(500, 19);
+            this.chkWpnHH.Name = "chkWpnHH";
+            this.chkWpnHH.Size = new System.Drawing.Size(89, 17);
+            this.chkWpnHH.TabIndex = 5;
+            this.chkWpnHH.Text = "Hunting Horn";
+            this.chkWpnHH.UseVisualStyleBackColor = true;
+            // 
+            // chkWpnLance
+            // 
+            this.chkWpnLance.AutoSize = true;
+            this.chkWpnLance.Location = new System.Drawing.Point(7, 44);
+            this.chkWpnLance.Name = "chkWpnLance";
+            this.chkWpnLance.Size = new System.Drawing.Size(56, 17);
+            this.chkWpnLance.TabIndex = 6;
+            this.chkWpnLance.Text = "Lance";
+            this.chkWpnLance.UseVisualStyleBackColor = true;
+            // 
+            // chkWpnGL
+            // 
+            this.chkWpnGL.AutoSize = true;
+            this.chkWpnGL.Location = new System.Drawing.Point(99, 44);
+            this.chkWpnGL.Name = "chkWpnGL";
+            this.chkWpnGL.Size = new System.Drawing.Size(72, 17);
+            this.chkWpnGL.TabIndex = 7;
+            this.chkWpnGL.Text = "Gunlance";
+            this.chkWpnGL.UseVisualStyleBackColor = true;
+            // 
+            // chkWpnSA
+            // 
+            this.chkWpnSA.AutoSize = true;
+            this.chkWpnSA.Location = new System.Drawing.Point(189, 44);
+            this.chkWpnSA.Name = "chkWpnSA";
+            this.chkWpnSA.Size = new System.Drawing.Size(79, 17);
+            this.chkWpnSA.TabIndex = 8;
+            this.chkWpnSA.Text = "Switch Axe";
+            this.chkWpnSA.UseVisualStyleBackColor = true;
+            // 
+            // chkWpnLBG
+            // 
+            this.chkWpnLBG.AutoSize = true;
+            this.chkWpnLBG.Location = new System.Drawing.Point(297, 44);
+            this.chkWpnLBG.Name = "chkWpnLBG";
+            this.chkWpnLBG.Size = new System.Drawing.Size(91, 17);
+            this.chkWpnLBG.TabIndex = 9;
+            this.chkWpnLBG.Text = "Light Bowgun";
+            this.chkWpnLBG.UseVisualStyleBackColor = true;
+            // 
+            // chkWpnHBG
+            // 
+            this.chkWpnHBG.AutoSize = true;
+            this.chkWpnHBG.Location = new System.Drawing.Point(395, 43);
+            this.chkWpnHBG.Name = "chkWpnHBG";
+            this.chkWpnHBG.Size = new System.Drawing.Size(99, 17);
+            this.chkWpnHBG.TabIndex = 10;
+            this.chkWpnHBG.Text = "Heavy Bowgun";
+            this.chkWpnHBG.UseVisualStyleBackColor = true;
+            // 
+            // chkWpnBow
+            // 
+            this.chkWpnBow.AutoSize = true;
+            this.chkWpnBow.Location = new System.Drawing.Point(500, 44);
+            this.chkWpnBow.Name = "chkWpnBow";
+            this.chkWpnBow.Size = new System.Drawing.Size(47, 17);
+            this.chkWpnBow.TabIndex = 11;
+            this.chkWpnBow.Text = "Bow";
+            this.chkWpnBow.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(939, 495);
+            this.Controls.Add(this.gbFilters);
             this.Name = "MainForm";
             this.Text = "Hunting List for MH3U";
+            this.gbFilters.ResumeLayout(false);
+            this.gbAmrFilters.ResumeLayout(false);
+            this.gbAmrFilters.PerformLayout();
+            this.gbWpnFilters.ResumeLayout(false);
+            this.gbWpnFilters.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox gbFilters;
+        private System.Windows.Forms.GroupBox gbWpnFilters;
+        private System.Windows.Forms.CheckBox chkWpnBow;
+        private System.Windows.Forms.CheckBox chkWpnHBG;
+        private System.Windows.Forms.CheckBox chkWpnLBG;
+        private System.Windows.Forms.CheckBox chkWpnSA;
+        private System.Windows.Forms.CheckBox chkWpnGL;
+        private System.Windows.Forms.CheckBox chkWpnLance;
+        private System.Windows.Forms.CheckBox chkWpnHH;
+        private System.Windows.Forms.CheckBox chkWpnHammer;
+        private System.Windows.Forms.CheckBox chkWpnDS;
+        private System.Windows.Forms.CheckBox chkWpnSnS;
+        private System.Windows.Forms.CheckBox chkWpnLS;
+        private System.Windows.Forms.CheckBox chkWpnGS;
+        private System.Windows.Forms.GroupBox gbAmrFilters;
+        private System.Windows.Forms.CheckBox chkAmrLegs;
+        private System.Windows.Forms.CheckBox chkAmrWaist;
+        private System.Windows.Forms.CheckBox chkAmrArms;
+        private System.Windows.Forms.CheckBox chkAmrBody;
+        private System.Windows.Forms.CheckBox chkAmrHead;
     }
 }
 
