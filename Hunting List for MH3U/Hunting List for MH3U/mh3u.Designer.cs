775a6366a94952d437be5b41b6e4d3b4c4c92114
ampedPF <ampedpf@gmail.com>
2014-07-07 23:59:23 +0200

    db added - about to delete .tt files

---
+++ Hunting List for MH3U/Hunting List for MH3U/mh3u.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'C:\DATA\PROJECTS\GitHub\Hunting-List-for-MH3U\Hunting List for MH3U\Hunting List for MH3U\mh3u.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
